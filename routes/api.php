487fc75dd86e99412fd82c58e750c3b07a6d4e68
Santiago de la Fuente <santiagodelafuente83@gmail.com>
2025-04-26 17:44:10 -0300

    Agrego github actions

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,10 @@ Route::get('/hello', function () {
     return response()->json([ 'status' => 'Hello', 'timestamp' => now() ]);
 });
 
+Route::get('/goodbye', function () {
+    return response()->json([ 'status' => 'Goodbye', 'timestamp' => now() ]);
+});
+
+
 Route::get('/positions', [PositionController::class, 'index'])->name('positions');
 Route::post('/positions', [PositionController::class, 'store'])->name('positions');
